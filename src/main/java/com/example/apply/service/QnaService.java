06b60c4b5a34d3cf6832c533b82fa9478f4aceae
cheonyeonsu <cheonyeonsu@gmail.com>
2023-07-29 22:29:54 +0900

    23.07.29

--- src/main/java/com/example/apply/service/QnaService.java
+++ src/main/java/com/example/apply/service/QnaService.java
@@ -2,6 +2,8 @@ package com.example.apply.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,135 +13,65 @@ import com.example.apply.entity.Qna;
 import com.example.apply.repository.MemberRepository;
 import com.example.apply.repository.QnaRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional // 임포트 주의. 클래스에 속하는 메서드에 공통적으로 적용
+@Transactional
 public class QnaService {
-	public Long saveQna(QnaDto qnaDto,String id) {
-		
-		private final QnaRepository qnaRepository;
-		private final MemberRepository memberRepository;
-		
+	
+	//의존성 주입
+	private final QnaRepository qnaRepository;
+	private final MemberRepository memberRepository;
+	
+	//게시글 저장 
+	public Long saveQna(QnaDto qnaDto, String id) {
 		Qna qna = qnaDto.createQna();
 		
 		Member member = memberRepository.findByUserId(id);
-		qna.setMember(member);package com.example.apply.controller;
-
-import java.security.Principal;
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.apply.dto.QnaDto;
-import com.example.apply.entity.Qna;
-import com.example.apply.service.QnaService;
-
-
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequiredArgsConstructor
-public class QnaController {
-	
-	private final QnaService qnaService;
-	
-  //게시글 화면 보여주기 + 페이징
-	@GetMapping(value={"/qna/list", "/qna/list/{page}"})
-  //Model:컨트롤러>뷰 데이터를 전달. 뷰 페이지와 폼 데이터 바인딩
-	public String list(Model model,QnaDto qnaDto, 
-                     @PathVariable("page") Optional<Integer> page) {
-		//한 페이지당 5개씩 보여주기
-		Pageable pageable = PageRequest.of(page.isPresent()?page.get():0, 5);
-		
-		Page<Qna> list = qnaService.getListPage(pageable);
-		
-		//숫자 5를 maxPage라는 이름으로 view에 보낸다
-		//model.addAttribute(String key, Object value);
-		model.addAttribute("lists",list);
-		model.addAttribute("maxPage",5);
-		
-		return "qna/qnaList";
-	}
-	
-	
-	@GetMapping(value="/qna/regist")
-	public String regist() {
-		return "qna/qnaRegist";
-	}
-	
-	@PostMapping(value = "/qna/regist")           
-	public String addRegist(@Valid QnaDto qnaDto, Principal principal) {
-												 //Principal:현재 인증된 사용자를 나타내는 인터페이스
-		String id = principal.getName();
-		
-		try {
-			qnaService.saveQna(qnaDto,id);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return "redirect:/qna/list";
-	}
-	
-	
-	@GetMapping(value = {"/qna/detail", "/qna/detail/{id}"})
-	public String detail(@PathVariable(name = "id") Long id,Model model) {																												
-		Qna qna = qnaService.getDetailPage(id);
-		
-		 model.addAttribute("qna", qna); 
-		 
-		return "qna/qnaDetail";
-	}
-	
-	//수정하기
-	@GetMapping(value={"/qna/modify","/qna/modify/{id}"})
-	public String edit(@PathVariable(name = "id") Long id, Model model) {
-		Qna qna = qnaService.getDetailPage(id);
-		
-		model.addAttribute("qnaDto", new QnaDto());
-		model.addAttribute("qna", qna); 
-		 
-		return "qna/qnaEdit";
-	}
-	
-	@PostMapping(value={"/qna/modify","/qna/modify/{id}"})
-	public String edit(@Valid QnaDto qnaDto, Principal principal,@PathVariable(name = "id") Long id) {
-		
-		qnaService.updateQna(id, qnaDto);
-		
-		//redirect : 지정된 다른 URL로 재요청하라고 지시
-		return "redirect:/qna/detail/" + id;
-	}
-	
-	//삭제하기 
-	@DeleteMapping("/qna/delete/{id}")
-	public @ResponseBody ResponseEntity deleteQna
-	(@PathVariable ("id") Long id,Principal principal) {
-		
-		qnaService.deleteQna(id);
-		
-		return new ResponseEntity<Long>(id,HttpStatus.OK);
-	}
-}
-		qna.setDate(LocalDateTime.now());
+		qna.setMember(member); //조인컬럼 넣어줌
+		qna.setDate(LocalDateTime.now()); //현재 시간 찍어줌.
 		
 		qnaRepository.save(qna);
 		
 		return qna.getId();
 	}
-
+	
+	@Transactional(readOnly = true) //데이터베이스 읽기 작업만 수행. 
+	public Page<Qna> getListPage(Pageable pageable){
+		Page<Qna> qnaPage = qnaRepository.findAll(pageable);
+		return qnaPage;
+		
+	}
+	
+	public Qna getDetailPage(Long id) {
+	
+		Qna qna = qnaRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+		/*orElseThrow(EntityNotFoundException::new): 오류처리.
+		 * Optional 객체가 비어있지 않을 경우, Optional이 감싸고 있는 값을 그대로 반환. 
+		 * Optional 객체가 비어있을 경우,EntityNotFoundException (또는 원하는 다른 예외)를 생성해 Throw
+		 */
+		return qna;
+		
+	}
+	
+	public Qna updateQna(Long id,QnaDto qnaDto) {
+		
+		Qna qna = qnaRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+		
+		qna.updateQna(qnaDto.getTitle(), qnaDto.getContent());
+		
+		return qna;
+	}
+	
+	public Qna deleteQna(Long id) {
+		
+		Qna qna = qnaRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+		
+		qnaRepository.delete(qna);
+		
+		return qna;
+	}
+	
 }
