9ab24d225cfdc71042f3d1f653b236ba1422cdab
cheonyeonsu <cheonyeonsu@gmail.com>
2023-08-08 22:26:26 +0900

    23.08.08

--- src/main/java/com/example/apply/entity/Schedule.java
+++ src/main/java/com/example/apply/entity/Schedule.java
@@ -1,33 +1,48 @@
 package com.example.apply.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-/*@Entity
+@Entity
 @Table(name = "schedule")
 @Getter
 @Setter
-@ToString*/
+@ToString
+//orderItem 참고.
 public class Schedule {
 
-	/*
 	@Id
-	@Column(name = "schedule_id") 
-	@GeneratedValue(strategy = GenerationType.AUTO) 
-	private Long ScheduleId;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id")
-	private Long MemberId;
-	 */
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "schedule_id")
+    private Long scheduleId;
+
+    @ManyToOne
+    @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+
+    @Column(nullable = false)
+    private String subjectName;
+
+    @Column(nullable = false)
+    private LocalDateTime subjectStartDate;
+
+    @Column(nullable = false)
+    private LocalDateTime subjectEndDate;
+    
 }
