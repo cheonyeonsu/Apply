0bfc3da2673464cbc90ed53017280579f73e0c20
cheonyeonsu <cheonyeonsu@gmail.com>
2023-10-26 22:59:04 +0900

    ID,PW FIX

--- src/main/java/com/example/apply/repository/ScheduleRepository.java
+++ src/main/java/com/example/apply/repository/ScheduleRepository.java
@@ -1,5 +1,12 @@
 package com.example.apply.repository;
 
-public interface ScheduleRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.apply.entity.Member;
+import com.example.apply.entity.Schedule;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>{
+	List<Schedule> findByMember(Member member);
 }
